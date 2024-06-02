3a94869263a450dfd56ae0527b9bde914446c4a9
unknown <lorelailyons@protonmail.com>
2024-06-02 02:54:07 -0400

    Not much progress

--- class-reg-seesaw/src/class_reg_seesaw/core.clj
+++ class-reg-seesaw/src/class_reg_seesaw/core.clj
@@ -1,20 +1,23 @@
 (ns class-reg-seesaw.core
   (:use seesaw.core)
   (:use seesaw.dev)
-  (:require [clojure.java.io :as io]
+  (:require [clojure.java.io :as _io]
             [seesaw.font :as _f]))
 
 (defn make-content []
-  (vertical-panel
-   :items [(label :text "Class-Reg" :font (_f/font :size 24) :h-text-position :center)
-           "Welcome to my class registration application"]))
+  (border-panel
+   :north (label :text "Class-Reg" :font (_f/font :size 24) :h-text-position :center)
+   :center "Login Here"))
 
 (defn main-frame []
-  (let [frame (frame :title "U Class-Reg-Clj"
-                    :minimum-size [600 :by 800]
-                    :on-close :exit)
-       content (make-content)]
-   (config! frame :content content)
+  (let [frame (frame
+               :id :m-frame
+               :title "U Class-Reg-Clj"
+               :content (make-content)
+               :minimum-size [600 :by 800]
+               :on-close :exit)
+        background-image (label :icon (_io/resource "field.jpg"))]
+   ;(config! :m-frame :position )
    frame))
 
 (defn -main [& args]
