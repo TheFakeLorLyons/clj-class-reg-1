c5fc2a05c961f3a2df3ad52d7655e8004da559c1
unknown <lorelailyons@protonmail.com>
2024-06-02 12:42:24 -0400

    Fixed formatting.

--- class-reg-seesaw/src/class_reg_seesaw/core.clj
+++ class-reg-seesaw/src/class_reg_seesaw/core.clj
@@ -4,10 +4,36 @@
   (:require [clojure.java.io :as _io]
             [seesaw.font :as _f]))
 
+(defn login-screen-header []
+  (let [header-content   
+        (vertical-panel 
+         :items [(label :text "Class-Reg"
+                        :font (_f/font :size 24)
+                        :h-text-position :center)
+                 (label :text "Welcome to the app"
+                        :font (_f/font :size 18)
+                        :h-text-position :center)])]
+    header-content))
+
+(defn login-screen-form []
+  (let [login-form
+        (vertical-panel
+         :items [(label :text "Login Here"
+                        :h-text-position :center)])]
+    login-form))
+
+(defn main-screen-contains []
+  (let [conjoined-panel
+        (border-panel
+         :north (login-screen-header)
+         :center (login-screen-form))]
+    conjoined-panel))
+
 (defn make-content []
-  (border-panel
-   :north (label :text "Class-Reg" :font (_f/font :size 24) :h-text-position :center)
-   :center "Login Here"))
+  (let [content-panel
+        (flow-panel
+         :items [(main-screen-contains)])]
+    content-panel))
 
 (defn main-frame []
   (let [frame (frame
@@ -16,8 +42,7 @@
                :content (make-content)
                :minimum-size [600 :by 800]
                :on-close :exit)
-        background-image (label :icon (_io/resource "field.jpg"))]
-   ;(config! :m-frame :position )
+        background-image (label :icon (_io/resource "field.jpg"))] 
    frame))
 
 (defn -main [& args]
